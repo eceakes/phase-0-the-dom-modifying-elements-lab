599ff5a5e5202d06db6a51cc32c7f9b468dc4321
Eric <eceakes@gmail.com>
2024-04-08 14:07:42 -0400

    DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('main');
+mainElement.removeAttribute('id');
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Eric is the champion";
+document.body.append(newHeader);
